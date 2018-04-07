859c9cdd6111e09649b58b9b9c5fcfc0b6e9f897
Lee4Ever <594540634@qq.com>
2018-04-07 22:56:04 +0800

    add files and some changes

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 //页面头部所有属性及方法
 var appHead = {
   goback: function(){
-    alert('跳转到上一级')
+    alert('common.js:3')
     //window.history.go(-1)
   },
   goMePage: function(){
-    alert('跳转到我的页面')
-    window.location='http://www.baidu.com'
+    alert('common.js:7')
+    //window.location='http://www.baidu.com'
   }
 }
