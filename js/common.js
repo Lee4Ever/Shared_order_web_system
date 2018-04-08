5c11ed10e2f18c1674b6773ee155264afeb20a1c
Lee4Ever <594540634@qq.com>
2018-04-08 16:52:16 +0800

    added and modified files

--- js/common.js
+++ js/common.js
@@ -1,17 +1,61 @@
 //页面头部所有属性及方法
 var appHead = {
-  goback: function(){
-    alert('common.js:3')
+  //头部返回按钮
+  goback() {
+    console.log('appHead.goback()')
     //window.history.go(-1)
   },
-  goMePage: function(){
-    alert('common.js:7')
+  //头部个人中心按钮
+  goMePage() {
+    console.log('appHead.goMePage()')
     //window.location='http://www.baidu.com'
   }
 }
 
+//简单页面方法
 var smPage = {
-  useCoupon: function(){
-    alert('common.js:15')
+  //使用现金券按钮方法
+  useCoupon() {
+    console.log('smPage.useCoupon()');
+  }
+}
+
+//表单页面方法
+var formPage = {
+  //验证码图片点击更换
+  yzmChange() {
+    console.log('formPage.yzmChange()');
+  },
+  //登录按钮方法
+  login() {
+    console.log('formPage.login()');
+  },
+  //获取短信验证码按钮
+  getSmsYzm() {
+    console.log('formPage.getSmsYzm()');
+    let _t = $('.sms_yzm_btn');
+    if (!_t.attr('disabled')) {
+      _t.text('重新发送(60s)');
+      _t.attr('disabled', true)
+      let t = 59;
+      let s = setInterval(function() {
+        if (t != 0) {
+          _t.text('重新发送(' + t + 's)');
+          --t;
+        } else {
+          clearInterval(s);
+          _t.attr('disabled', false)
+          _t.text('发送验证码');
+        }
+      }, 1000)
+    }
+  },
+  //找回密码按钮方法
+  findPwd() {
+    console.log('formPage.findPwd()');
+  },
+  //支付宝账户绑定按钮
+  alipayBind() {
+    console.log('formPage.alipayBind()')
   }
 }
