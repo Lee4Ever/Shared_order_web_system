319263de8cc0ea0743399660742d15e26705709c
Lee <594540634@qq.com>
2018-04-09 19:21:05 +0800

    new files

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 //页面头部所有属性及方法
-let appHead = {
+const appHead = {
   //头部返回按钮
   goback() {
     console.log('appHead.goback()')
@@ -13,7 +13,7 @@ let appHead = {
 }
 
 //简单页面方法
-let smPage = {
+const smPage = {
   //使用现金券按钮方法
   useCoupon() {
     console.log('smPage.useCoupon()');
@@ -21,7 +21,7 @@ let smPage = {
 }
 
 //表单页面方法
-let formPage = {
+const formPage = {
   //验证码图片点击更换
   yzmChange() {
     console.log('formPage.yzmChange()');
@@ -61,5 +61,51 @@ let formPage = {
   //用户注册按钮
   register() {
     console.log('formPage.register()')
+  }
+}
+
+//列表页面方法
+const list = {
+  switchTab(_t){
+    console.log('list.switchTab()');
+    let n = _t.index()+1;
+    $('.single_tab:nth-child('+n+')').addClass('f-g').siblings().removeClass('f-g');
+    $('.tab_page:nth-child('+n+')').show().siblings().hide();
+  },
+  search(){
+    console.log('list.search()');
+    v = $('#search_bar').val();
+    if(v==''){
+      alert('请输入您要搜索的内容！')
+    } else {
+      alert('搜索'+v)
+    }
+  },
+  fns(cname){
+    switch (cname) {
+      case 'f_accept':
+        console.log('新朋友接受');
+        break;
+      case 'f_refuse':
+        console.log('新朋友拒绝');
+        break;
+      case 'f_del':
+        console.log('删除朋友');
+        break;
+      case 's_accept':
+        console.log('新合作店家接受');
+        break;
+      case 's_refuse':
+        console.log('新合作店家拒绝');
+        break;
+      case 's_del':
+        console.log('删除合作店家');
+        break;
+      case 's_view':
+        console.log('查看合作店家');
+        break;
+      default:
+        console.log('button指令获取错误');
+    }
   }
 }
